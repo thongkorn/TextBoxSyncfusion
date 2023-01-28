e78f9ac556d55ba0eef95d163b32de8064dfad57
Thongkorn Tubtimkrob <thongkorn@hotmail.com>
2023-01-28 14:34:43 +0700

    Add files via upload

---
+++ TextBoxSyncfusion/TextBoxSyncfusion/frmTextBoxSyncfusion.Designer.vb
@@ -0,0 +1,273 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmTextBoxSyncfusion
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txtDouble = New System.Windows.Forms.TextBox()
+        Me.txtInteger = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBoxExt1 = New Syncfusion.Windows.Forms.Tools.TextBoxExt()
+        Me.DoubleTextBox2 = New Syncfusion.Windows.Forms.Tools.DoubleTextBox()
+        Me.PercentTextBox1 = New Syncfusion.Windows.Forms.Tools.PercentTextBox()
+        Me.DoubleTextBox1 = New Syncfusion.Windows.Forms.Tools.DoubleTextBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.TextBoxExt1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DoubleTextBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PercentTextBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DoubleTextBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtDouble)
+        Me.GroupBox1.Controls.Add(Me.txtInteger)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 0)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(454, 150)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = " Microsoft "
+        '
+        'txtDouble
+        '
+        Me.txtDouble.Location = New System.Drawing.Point(168, 85)
+        Me.txtDouble.Name = "txtDouble"
+        Me.txtDouble.Size = New System.Drawing.Size(187, 27)
+        Me.txtDouble.TabIndex = 1
+        Me.txtDouble.Text = "25658.451"
+        Me.txtDouble.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'txtInteger
+        '
+        Me.txtInteger.Location = New System.Drawing.Point(168, 43)
+        Me.txtInteger.Name = "txtInteger"
+        Me.txtInteger.Size = New System.Drawing.Size(187, 27)
+        Me.txtInteger.TabIndex = 0
+        Me.txtInteger.Text = "123456"
+        Me.txtInteger.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(45, 88)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(103, 19)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Double Value"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(45, 46)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(104, 19)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Integer Value"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label7)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.TextBoxExt1)
+        Me.GroupBox2.Controls.Add(Me.DoubleTextBox2)
+        Me.GroupBox2.Controls.Add(Me.PercentTextBox1)
+        Me.GroupBox2.Controls.Add(Me.DoubleTextBox1)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 156)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(454, 234)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = " Syncfusion "
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Label7.Location = New System.Drawing.Point(318, 90)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(113, 14)
+        Me.Label7.TabIndex = 12
+        Me.Label7.Text = "(แสดงเลขจำนวนเต็ม)"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(46, 182)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(118, 19)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "PercentTextBox"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(46, 136)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(116, 19)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "DoubleTextBox"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(46, 87)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(116, 19)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "DoubleTextBox"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(46, 42)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(88, 19)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "TextBoxExt"
+        '
+        'TextBoxExt1
+        '
+        Me.TextBoxExt1.BackColor = System.Drawing.Color.FromArgb(CType(CType(234, Byte), Integer), CType(CType(242, Byte), Integer), CType(CType(251, Byte), Integer))
+        Me.TextBoxExt1.BeforeTouchSize = New System.Drawing.Size(146, 27)
+        Me.TextBoxExt1.BorderColor = System.Drawing.Color.FromArgb(CType(CType(188, Byte), Integer), CType(CType(208, Byte), Integer), CType(CType(229, Byte), Integer))
+        Me.TextBoxExt1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBoxExt1.CornerRadius = 5
+        Me.TextBoxExt1.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.TextBoxExt1.Location = New System.Drawing.Point(168, 39)
+        Me.TextBoxExt1.MinimumSize = New System.Drawing.Size(14, 10)
+        Me.TextBoxExt1.Name = "TextBoxExt1"
+        Me.TextBoxExt1.Size = New System.Drawing.Size(146, 27)
+        Me.TextBoxExt1.Style = Syncfusion.Windows.Forms.Tools.TextBoxExt.theme.Office2007
+        Me.TextBoxExt1.TabIndex = 0
+        Me.TextBoxExt1.Text = "ABCกขค"
+        Me.TextBoxExt1.ThemeName = "Office2007"
+        '
+        'DoubleTextBox2
+        '
+        Me.DoubleTextBox2.BackGroundColor = System.Drawing.Color.FromArgb(CType(CType(237, Byte), Integer), CType(CType(245, Byte), Integer), CType(CType(253, Byte), Integer))
+        Me.DoubleTextBox2.BeforeTouchSize = New System.Drawing.Size(146, 27)
+        Me.DoubleTextBox2.BorderColor = System.Drawing.Color.FromArgb(CType(CType(209, Byte), Integer), CType(CType(211, Byte), Integer), CType(CType(212, Byte), Integer))
+        Me.DoubleTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.DoubleTextBox2.CornerRadius = 5
+        Me.DoubleTextBox2.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.DoubleTextBox2.DoubleValue = 27589.52R
+        Me.DoubleTextBox2.Location = New System.Drawing.Point(168, 133)
+        Me.DoubleTextBox2.MinimumSize = New System.Drawing.Size(14, 10)
+        Me.DoubleTextBox2.Name = "DoubleTextBox2"
+        Me.DoubleTextBox2.Size = New System.Drawing.Size(146, 27)
+        Me.DoubleTextBox2.Style = Syncfusion.Windows.Forms.Tools.TextBoxExt.theme.Metro
+        Me.DoubleTextBox2.TabIndex = 2
+        Me.DoubleTextBox2.Text = "27,589.52"
+        Me.DoubleTextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        Me.DoubleTextBox2.ThemeName = "Metro"
+        '
+        'PercentTextBox1
+        '
+        Me.PercentTextBox1.BackGroundColor = System.Drawing.Color.FromArgb(CType(CType(237, Byte), Integer), CType(CType(245, Byte), Integer), CType(CType(253, Byte), Integer))
+        Me.PercentTextBox1.BeforeTouchSize = New System.Drawing.Size(146, 27)
+        Me.PercentTextBox1.BorderColor = System.Drawing.Color.FromArgb(CType(CType(188, Byte), Integer), CType(CType(202, Byte), Integer), CType(CType(221, Byte), Integer))
+        Me.PercentTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PercentTextBox1.CornerRadius = 5
+        Me.PercentTextBox1.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.PercentTextBox1.Location = New System.Drawing.Point(168, 178)
+        Me.PercentTextBox1.MinimumSize = New System.Drawing.Size(14, 10)
+        Me.PercentTextBox1.Name = "PercentTextBox1"
+        Me.PercentTextBox1.Size = New System.Drawing.Size(146, 27)
+        Me.PercentTextBox1.Style = Syncfusion.Windows.Forms.Tools.TextBoxExt.theme.Office2010
+        Me.PercentTextBox1.TabIndex = 3
+        Me.PercentTextBox1.Text = "0.00%"
+        Me.PercentTextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        Me.PercentTextBox1.ThemeName = "Office2010"
+        '
+        'DoubleTextBox1
+        '
+        Me.DoubleTextBox1.BackGroundColor = System.Drawing.Color.FromArgb(CType(CType(237, Byte), Integer), CType(CType(245, Byte), Integer), CType(CType(253, Byte), Integer))
+        Me.DoubleTextBox1.BeforeTouchSize = New System.Drawing.Size(146, 27)
+        Me.DoubleTextBox1.BorderColor = System.Drawing.Color.FromArgb(CType(CType(188, Byte), Integer), CType(CType(202, Byte), Integer), CType(CType(221, Byte), Integer))
+        Me.DoubleTextBox1.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.DoubleTextBox1.DoubleValue = -2565.0R
+        Me.DoubleTextBox1.ForeColor = System.Drawing.Color.Red
+        Me.DoubleTextBox1.Location = New System.Drawing.Point(168, 84)
+        Me.DoubleTextBox1.MinimumSize = New System.Drawing.Size(14, 10)
+        Me.DoubleTextBox1.Name = "DoubleTextBox1"
+        Me.DoubleTextBox1.Size = New System.Drawing.Size(146, 27)
+        Me.DoubleTextBox1.TabIndex = 1
+        Me.DoubleTextBox1.Text = "-2,565.00"
+        Me.DoubleTextBox1.ThemeName = "Default"
+        '
+        'frmTextBoxSyncfusion
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 19.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(478, 396)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmTextBoxSyncfusion"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "TextBox Control of Syncfusion"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        CType(Me.TextBoxExt1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DoubleTextBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PercentTextBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DoubleTextBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents txtDouble As System.Windows.Forms.TextBox
+    Friend WithEvents txtInteger As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TextBoxExt1 As Syncfusion.Windows.Forms.Tools.TextBoxExt
+    Friend WithEvents DoubleTextBox2 As Syncfusion.Windows.Forms.Tools.DoubleTextBox
+    Friend WithEvents PercentTextBox1 As Syncfusion.Windows.Forms.Tools.PercentTextBox
+    Friend WithEvents DoubleTextBox1 As Syncfusion.Windows.Forms.Tools.DoubleTextBox
+
+End Class
---
+++ TextBoxSyncfusion/TextBoxSyncfusion/frmTextBoxSyncfusion.vb
@@ -0,0 +1,100 @@
+﻿#Region "About"
+' / --------------------------------------------------------------------------------
+' / Developer : Mr.Surapon Yodsanga (Thongkorn Tubtimkrob)
+' / eMail : thongkorn@hotmail.com
+' / URL: http://www.g2gnet.com (Khon Kaen - Thailand)
+' / Facebook: https://www.facebook.com/g2gnet (For Thailand)
+' / Facebook: https://www.facebook.com/commonindy (Worldwide)
+' / More Info: http://www.g2gsoft.com/
+' /
+' / Purpose: TextBox Control Extend from Syncfusion Community License Free.
+' / Microsoft Visual Basic .NET (2010)
+' /
+' / This is open source code under @CopyLeft by Thongkorn Tubtimkrob.
+' / You can modify and/or distribute without to inform the developer.
+' / --------------------------------------------------------------------------------
+#End Region
+
+Public Class frmTextBoxSyncfusion
+
+    Private Sub frmTextBoxSyncfusion_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        '// ตัวอย่างการกำหนดคุณสมบัติให้กับ Control ของ Syncfusion
+        '// สามารถกำหนดในแบบ Design Time เลยจะง่ายกว่า
+        With DoubleTextBox1
+            '// จะต้องกำหนดให้เป็น FixedSingle ถึงจะทำมุม (CornerRadius) ได้
+            .BorderStyle = BorderStyle.FixedSingle
+            .CornerRadius = 0
+            '// ใช้ DoubleTextBox เพื่อรับค่าเลขจำนวนเต็ม
+            .NumberDecimalDigits = 0
+            '// ให้แสดงเครื่องหมาย - นำหน้าเลขลบ (ค่าเริ่มต้นเป็น 0 ทำให้แสดงเครื่องหมายวงเล็บ)
+            .NumberNegativePattern = 1
+            .TextAlign = HorizontalAlignment.Right  '// จัดชิดขวา
+            .Style = Syncfusion.Windows.Forms.Tools.TextBoxExt.theme.Office2007 '// Theme
+            .PositiveColor = Color.Blue '// สีเลขบวก
+            .ZeroColor = Color.Red      '// สีเลขลบ
+        End With
+    End Sub
+
+    ' / --------------------------------------------------------------------------------
+    ' / เหตุการณ์กด KeyPress เพื่อดักคีย์ Enter
+    ' / --------------------------------------------------------------------------------
+    Private Sub DoubleTextBox1_KeyPress(sender As Object, e As System.Windows.Forms.KeyPressEventArgs) Handles DoubleTextBox1.KeyPress
+        '// เมื่อกด Enter ให้โฟกัสไปยัง Control ตัวถัดไป
+        If Asc(e.KeyChar) = Keys.Enter Then SendKeys.Send("{TAB}")
+    End Sub
+
+
+    ' / --------------------------------------------------------------------------------
+    ' / ฟังค์ชั่นที่ใช้งานกับ TextBox Control ของ Microsoft
+    ' / --------------------------------------------------------------------------------
+    ' / ฟังค์ชั่นในการป้อนเฉพาะค่าตัวเลขได้เท่านั้น
+    Function CheckDigitOnly(ByVal index As Integer) As Boolean
+        Select Case index
+            Case 48 To 57 ' เลข 0 - 9
+                CheckDigitOnly = False
+            Case 8, 13 ' Backspace = 8, Enter = 13
+                CheckDigitOnly = False
+            Case Else
+                CheckDigitOnly = True
+        End Select
+    End Function
+
+    ' / --------------------------------------------------------------------------------
+    ' / ฟังค์ชั่นในการป้อนเฉพาะค่าตัวเลขและทศนิยมได้ตัวเดียวเท่านั้น
+    Function CheckCurrency(index As Integer, tmpStr As String) As Boolean
+        CheckCurrency = False
+        Select Case index
+            Case 48 To 57 ' เลข 0 - 9
+                ' Allowed "."
+            Case 46
+                ' can present "." only one
+                If InStr(tmpStr, ".") Then CheckCurrency = True
+
+            Case 8, 13 ' Backspace = 8, Enter = 13
+            Case Else
+                CheckCurrency = True
+        End Select
+    End Function
+
+    ' / --------------------------------------------------------------------------------
+    ' / ต้องเพิ่มโค้ดในการดักการกดคีย์จากผู้ใช้ (Validate Data)
+    Private Sub txtInteger_KeyPress(sender As Object, e As System.Windows.Forms.KeyPressEventArgs) Handles txtInteger.KeyPress
+        If Asc(e.KeyChar) = Keys.Enter Then
+            e.Handled = True
+            SendKeys.Send("{TAB}")
+        Else
+            e.Handled = CheckDigitOnly(Asc(e.KeyChar))
+        End If
+    End Sub
+
+    Private Sub txtDouble_KeyPress(sender As Object, e As System.Windows.Forms.KeyPressEventArgs) Handles txtDouble.KeyPress
+        If Asc(e.KeyChar) = Keys.Enter Then
+            e.Handled = True
+            SendKeys.Send("{TAB}")
+        Else
+            e.Handled = CheckCurrency(Asc(e.KeyChar), txtDouble.Text)
+        End If
+    End Sub
+    ' / --------------------------------------------------------------------------------
+
+End Class
